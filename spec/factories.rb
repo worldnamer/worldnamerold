5ef94eb7eb57cf3c25011cdd92292b25bd1687b0
John LeBoeuf-Little <worldnamer@worldnamer.com>
2015-03-24 00:44:34 -0600

    Correct an oversight with the password_confirmation field in the user factory.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
     password 'password'
-    password_confirmation 'password_confirmation'
+    password_confirmation 'password'
 
     factory :worldnamer do
       email 'worldnamer@worldnamer.com'
