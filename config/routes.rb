e6462f525f0f09d776bf8c4fd7163ed514d9b699
John LeBoeuf-Little <worldnamer@worldnamer.com>
2015-06-17 00:15:13 -0600

    More minor tweaks - default routing for unauthenticated users, and removing dead spec.

--- config/routes.rb
+++ config/routes.rb
@@ -7,29 +7,30 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy', as: :get_destroy_user_session
   end
 
-  resources :projects, only: [:index, :new, :create, :show, :destroy, :update] do
-    resources :sections, only: [:new, :create, :destroy, :update] do
-      resources :todos, only: [:new, :create, :update, :destroy]
-      resources :links, only: [:new, :create, :destroy], controller: 'snippets'
-    end
-  end
-
-  resources :goals, only: [:index, :new, :create, :destroy, :update] do
-    member do
-      post :complete
-    end
-  end
-
-  get '/media' => 'media#index', as: :media
-
-  resources :links, only: [:new, :create, :destroy]
-
   authenticated :user do
+    resources :projects, only: [:index, :new, :create, :show, :destroy, :update] do
+      resources :sections, only: [:new, :create, :destroy, :update] do
+        resources :todos, only: [:new, :create, :update, :destroy]
+        resources :links, only: [:new, :create, :destroy], controller: 'snippets'
+      end
+    end
+
+    resources :goals, only: [:index, :new, :create, :destroy, :update] do
+      member do
+        post :complete
+      end
+    end
+
+    get '/media' => 'media#index', as: :media
+
+    resources :links, only: [:new, :create, :destroy]
+
     root to: 'welcome#main', as: :main
     get '/export' => 'shipping#export', as: :export
   end
 
   unauthenticated do
     root to: 'welcome#index'
+    get '*path' => redirect('/')
   end
 end
